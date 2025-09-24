fb58ee620ede2768fdcce1a9b1b3febe7b9bf071
Phyu Phyu Thin <phyuthin2004@gmail.com>
2025-09-24 18:36:30 +0630

    [Added] Role BL and DA service registrations

--- HRSystem.Csharp.Domain/FeatureManager.cs
+++ HRSystem.Csharp.Domain/FeatureManager.cs
@@ -1,14 +1,32 @@
-﻿namespace HRSystem.Csharp.Domain
+﻿using HRSystem.Csharp.Domain.Features;
+
+namespace HRSystem.Csharp.Domain
 {
     public static class FeatureManager
     {
+        
+        private static void AddServices(this WebApplicationBuilder builder)
+        {
+            #region User Management BL
+
+            builder.Services.AddScoped<BL_Role>();
+
+            #endregion
+
+            #region User Management DA
+
+            builder.Services.AddScoped<DA_Role>();
+
+            #endregion
+        }
+        
         public static void AddDomain(this WebApplicationBuilder builder)
         {
             builder.Services.AddDbContext<AppDbContext>(opt =>
             {
                 opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
             }, ServiceLifetime.Transient, ServiceLifetime.Transient);
-
+            
         }
 
 
