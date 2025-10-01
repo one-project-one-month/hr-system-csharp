ae2c5f5a3b2e3976878eb09844ff673f8de148ea
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2025-10-01 22:56:06 +0700

    feat: Add controller for Attendance and add service

---
+++ HRSystem.Csharp.Api/Controllers/AttendanceConroller.cs
@@ -0,0 +1,25 @@
+﻿using HRSystem.Csharp.Domain.Features.Attendance;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRSystem.Csharp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendanceConroller : ControllerBase
+    {
+        private readonly BL_Attendance _bL_Attendance;
+
+        public AttendanceConroller(BL_Attendance bL_Attendance)
+        {
+            _bL_Attendance = bL_Attendance;
+        }
+
+        [HttpGet("AttendanceList")]
+        public async Task<IActionResult> AttendanceLists()
+        {
+            var data = await _bL_Attendance.GetAllAttendances();
+            return Ok(data);
+        }
+    }
+}
--- HRSystem.Csharp.Domain/Features/Attendance/DA_Attendance.cs
+++ HRSystem.Csharp.Domain/Features/Attendance/DA_Attendance.cs
@@ -19,16 +19,31 @@ namespace HRSystem.Csharp.Domain.Features.Attendance
 
         public async Task<Result<AttendanceListResponseModel>> GetAllAttendances()
         {
-            var attendanceList = await _db.TblAttendances.Where(x => x.DeleteFlag == false)
+            try
+            {
+                var attendanceList = await _db.TblAttendances.Where(x => x.DeleteFlag == false)
                 .OrderByDescending(x => x.AttendanceDate)
                 .ToListAsync();
 
-            var model = new AttendanceListResponseModel
+                if (!attendanceList.Any() || attendanceList is null)
+                    return Result<AttendanceListResponseModel>.NotFoundError("No attendance found.");
+
+                var model = new AttendanceListResponseModel
+                {
+                    AttendanceList = attendanceList
+                    .Select(AttendanceListModel.FromTblAttendance)
+                    .ToList()
+                };
+
+                return Result<AttendanceListResponseModel>.Success(model);
+            }
+            catch (Exception ex)
             {
-                AttendanceList = attendanceList
-                .Select(AttendanceListModel.FromTblAttendance)
-                .ToList()
-            };
+                return Result<AttendanceListResponseModel>.SystemError(ex.Message);
+               
+            }
+
+            
 
         }
     }
