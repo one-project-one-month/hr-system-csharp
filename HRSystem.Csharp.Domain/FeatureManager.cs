9d34799d84494f21afb4099a596739af38c9672b
sawmonhan <sawmonhan@vitheymfi.com>
2025-11-10 11:52:50 +0630

    database error

--- HRSystem.Csharp.Domain/FeatureManager.cs
+++ HRSystem.Csharp.Domain/FeatureManager.cs
@@ -79,23 +79,24 @@ public static class FeatureManager
     {
         Env.Load(".env.development");
 
-        var host = Environment.GetEnvironmentVariable("DB_HOST");
+        var host = Environment.GetEnvironmentVariable("DB_HOST") ?? "127.0.0.1";
         //var port = Environment.GetEnvironmentVariable("DB_PORT");
         var db = Environment.GetEnvironmentVariable("DB_NAME");
         var user = Environment.GetEnvironmentVariable("DB_USER");
         var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
 
-        var mssqlConnection = $"Server={host};Database={db};User Id={user};Password={password};TrustServerCertificate=True";
+        var mssqlConnection = $"Server=tcp:{host},1433;Database={db};User Id={user};Password={password};TrustServerCertificate=True";
 
         builder.Services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(mssqlConnection));
-        builder.Services.AddDbContext<AppDbContext>(opt => { opt.UseSqlServer(mssqlConnection)
-            .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking); },
-            ServiceLifetime.Transient, 
-            ServiceLifetime.Transient);
+            options.UseSqlServer(mssqlConnection,
+            sqlOptions => sqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null))
+              .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+        );
 
-        builder.Services.AddTransient<IDbConnection, SqlConnection>(n =>
-            new SqlConnection(mssqlConnection));
+        builder.Services.AddScoped<IDbConnection>(sp =>
+        {
+            return new SqlConnection(mssqlConnection); // connection will open lazily when Dapper executes
+        });
 
         builder.Services
             .AddFluentEmail("hrsystem.opom@gmail.com")
